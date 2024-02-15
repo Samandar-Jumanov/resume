0907c5eabef5e9b9008fea9aae7dddaa744397f1
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-02-15 06:45:36 -0500

    adding about part

--- app/page.js
+++ app/page.js
@@ -1,14 +1,25 @@
-// pages/index.js
 "use client"
+
 import Head from 'next/head';
 import { useRouter } from 'next/navigation';
+import { useState , useEffect} from "react"
+import ProjectCard from '@/components/ProjectCard'; 
+import { getProjects } from '@/components/fetchProjects'; 
 
 export default function Home() {
   const router = useRouter();
+  const [projects, setProjects] = useState([]);
 
+  useEffect(() => {
+    getProjects()
+      .then((data) => { 
+           setProjects(data);
+      })
+      .catch(error => console.error('Error fetching projects:', error));
+  }, []);
 
   function downloadPage(){
-         router.push("download")
+    router.push("download");
   };
 
   return (
@@ -26,18 +37,42 @@ export default function Home() {
       </header>
 
       <main className="container mx-auto px-4 py-8 text-blue-200">
-        <h1 className="text-3xl font-bold mb-4">Hello, I'm [Your Name]</h1>
-        <p className="text-lg mb-8">[A brief introduction about yourself]</p>
+  <h1 className="text-3xl font-bold mb-4">Hello, I'm Samandar</h1>
+  <p className="text-lg mb-8">
+    🚀 Skilled Backend Developer | Full Stack Enthusiast | JavaScript | TypeScript | Python | Go
+    <br />
+    <br />
+    Passionate about crafting robust backend solutions and integrating them seamlessly with cutting-edge frontend technologies. With a diverse skill set and 1.5 years of project-based experience, I excel in building scalable web applications and microservices architectures.
+    <br />
+    <br />
+    🔧 Technologies & Tools:
+    <br />
+    <ul>
+      <li>Backend: Node.js | TypeScript | Python | Go | Express.js | Socket.io | RabbitMQ</li>
+      <li>Frontend: React.js | Next.js | JavaScript</li>
+      <li>DevOps: Docker | Kubernetes</li>
+      <li>Databases: MongoDB | MySQL</li>
+      <li>Messaging: RabbitMQ</li>
+      <li>Microservices Architecture</li>
+    </ul>
+    <br />
+    🌟 Highlights:
+    <br />
+    <ul>
+      <li>Proficient in backend development with Node.js, TypeScript, Python, and Go, ensuring high performance and reliability.</li>
+      <li>Experienced in building frontend interfaces with React.js and Next.js, ensuring a seamless user experience.</li>
+      <li>Skilled in containerization and orchestration with Docker and Kubernetes, optimizing deployment processes.</li>
+      <li>Familiar with messaging systems like RabbitMQ for building scalable and resilient microservices architectures.</li>
+      <li>Actively participate in problem-solving on platforms like LeetCode (https://leetcode.com/jumanov/), honing algorithmic and coding skills.</li>
+    </ul>
+  </p>
 
-        <h2 className="text-2xl font-bold mb-4">Personal Projects</h2>
+  <h2 className="text-2xl font-bold mb-4">Personal Projects</h2>
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> 
+    <ProjectCard projects={projects} />
+  </div>
+</main>
 
-      </main>
-
-      <footer className="bg-gray-800 py-6">
-        <div className="container mx-auto px-4 text-center">
-          <p className="text-white">&copy; {new Date().getFullYear()} My Resume Landing Page. All Rights Reserved.</p>
-        </div>
-      </footer>
     </div>
   );
 }
--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,25 +1,24 @@
-"use client"
+import React from 'react';
 
-import React from 'react'
+const ProjectCard = ({ projects }) => {
+  function directUrl(url) {
+    window.open(url, '_blank');
+  }
 
-const ProjectCard = ( projects ) => {
   return (
-       <>
-
-      {projects.map((each) =>{
-            return (
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" >
-                <div className="bg-blue-900 rounded-lg shadow-md p-6">
-            <h3 className="text-xl font-semibold mb-2">Project 1</h3>
-            <p className="mb-4">Description of Project 1</p>
-            <a href="#" className="text-cyan-400 hover:underline">View Project</a>
+    <>
+      {projects.map((each) => (
+        <div className="bg-blue-900 rounded-lg shadow-md p-6 mb-4" key={each.id}>
+          <h3 className="text-white text-xl font-semibold mb-2">Project name: {each.name}</h3>
+          <p className="text-white mb-4">GitHub URL: <a href={each.html_url} target="_blank" rel="noopener noreferrer" className="text-cyan-400 hover:underline">{each.html_url}</a></p>
+          <p>Created at : {each.created_at}</p>
+          <button onClick={() => directUrl(`https://github.com/Samandar-Jumanov/${each.name}`)} className="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            See project 
+          </button>
         </div>
-                </div>
-            )
-      })}
-       
-       </>
-  )
-}
+      ))}
+    </>
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
