97b0e008f9a3e12039d5941600182e78d2c921ea
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-02-15 06:58:52 -0500

    adding styles

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@
 
 import Head from 'next/head';
 import { useRouter } from 'next/navigation';
-import { useState , useEffect} from "react"
-import ProjectCard from '@/components/ProjectCard'; 
-import { getProjects } from '@/components/fetchProjects'; 
+import { useState, useEffect } from "react";
+import ProjectCard from '@/components/ProjectCard';
+import { getProjects } from '@/components/fetchProjects';
 
 export default function Home() {
   const router = useRouter();
@@ -13,7 +13,7 @@ export default function Home() {
   useEffect(() => {
     getProjects()
       .then((data) => { 
-           setProjects(data);
+        setProjects(data);
       })
       .catch(error => console.error('Error fetching projects:', error));
   }, []);
@@ -25,7 +25,7 @@ export default function Home() {
   return (
     <div className="bg-black min-h-screen">
       <Head>
-        <title>My Resume Landing Page</title>
+        <title>Samandar Jumanov</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -37,42 +37,45 @@ export default function Home() {
       </header>
 
       <main className="container mx-auto px-4 py-8 text-blue-200">
-  <h1 className="text-3xl font-bold mb-4">Hello, I'm Samandar</h1>
-  <p className="text-lg mb-8">
-    🚀 Skilled Backend Developer | Full Stack Enthusiast | JavaScript | TypeScript | Python | Go
-    <br />
-    <br />
-    Passionate about crafting robust backend solutions and integrating them seamlessly with cutting-edge frontend technologies. With a diverse skill set and 1.5 years of project-based experience, I excel in building scalable web applications and microservices architectures.
-    <br />
-    <br />
-    🔧 Technologies & Tools:
-    <br />
-    <ul>
-      <li>Backend: Node.js | TypeScript | Python | Go | Express.js | Socket.io | RabbitMQ</li>
-      <li>Frontend: React.js | Next.js | JavaScript</li>
-      <li>DevOps: Docker | Kubernetes</li>
-      <li>Databases: MongoDB | MySQL</li>
-      <li>Messaging: RabbitMQ</li>
-      <li>Microservices Architecture</li>
-    </ul>
-    <br />
-    🌟 Highlights:
-    <br />
-    <ul>
-      <li>Proficient in backend development with Node.js, TypeScript, Python, and Go, ensuring high performance and reliability.</li>
-      <li>Experienced in building frontend interfaces with React.js and Next.js, ensuring a seamless user experience.</li>
-      <li>Skilled in containerization and orchestration with Docker and Kubernetes, optimizing deployment processes.</li>
-      <li>Familiar with messaging systems like RabbitMQ for building scalable and resilient microservices architectures.</li>
-      <li>Actively participate in problem-solving on platforms like LeetCode (https://leetcode.com/jumanov/), honing algorithmic and coding skills.</li>
-    </ul>
-  </p>
+        <h1 className="text-3xl font-bold mb-4">Hello, I'm Samandar</h1>
+        <p className="text-lg mb-8">
+          🚀 Skilled Backend Developer | Full Stack Enthusiast | JavaScript | TypeScript | Python | Go
+          <br />
+          <br />
+          Passionate about crafting robust backend solutions and integrating them seamlessly with cutting-edge frontend technologies. With a diverse skill set and 1.5 years of project-based experience, I excel in building scalable web applications and microservices architectures.
+        </p>
 
-  <h2 className="text-2xl font-bold mb-4">Personal Projects</h2>
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> 
-    <ProjectCard projects={projects} />
-  </div>
-</main>
+        <div className="mb-8 p-4 bg-blue-500 rounded-lg shadow-md text-black">
+          <h2 className="text-2xl font-bold mb-2">Highlights</h2>
+          <ul>
+            <li className="flex items-center space-x-2 mb-2">
+              <svg className="w-6 h-6 text-yellow-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+              <span className="text-lg font-bold">Proficient in backend development with Node.js, TypeScript, Python, and Go, ensuring high performance and reliability.</span>
+            </li>
+            <li className="flex items-center space-x-2 mb-2">
+              <svg className="w-6 h-6 text-yellow-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+              <span className="text-lg font-bold">Experienced in building frontend interfaces with React.js and Next.js, ensuring a seamless user experience.</span>
+            </li>
+            <li className="flex items-center space-x-2 mb-2">
+              <svg className="w-6 h-6 text-yellow-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+              <span className="text-lg font-bold">Skilled in containerization and orchestration with Docker and Kubernetes, optimizing deployment processes.</span>
+            </li>
+            <li className="flex items-center space-x-2 mb-2">
+              <svg className="w-6 h-6 text-yellow-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+              <span className="text-lg font-bold">Familiar with messaging systems like RabbitMQ for building scalable and resilient microservices architectures.</span>
+            </li>
+            <li className="flex items-center space-x-2 mb-2">
+              <svg className="w-6 h-6 text-yellow-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+              <span className="text-lg font-bold">Actively participate in problem-solving on platforms like LeetCode (https://leetcode.com/jumanov/), honing algorithmic and coding skills.</span>
+            </li>
+          </ul>
+        </div>
 
+        <h2 className="text-2xl font-bold mb-4">Personal Projects</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> 
+          <ProjectCard projects={projects} />
+        </div>
+      </main>
     </div>
   );
 }
