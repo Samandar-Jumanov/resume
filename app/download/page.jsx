a7bb68a8bfcd540aba06c64e45f8a253cee6cd5c
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-02-15 12:04:11 -0500

    added detectOs

--- app/download/page.jsx
+++ app/download/page.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+"use client"
 
+import detectOs from '@/components/detectOs'
+import React , { useEffect , useState} from 'react'
+import detectOS from '@/components/detectOs'
 const Download = () => {
+
+    const [os , setOs ] = useState("Windows")
+     
+    useEffect(() => {
+      detectOS().then((res ) => setOs(res))
+      .catch((err ) => console.log(err))
+    }, []);
+
+
+
   return (
     <div>
         All files should be here 
